e3789ccb3d86faed951153eb4dafdfc1da2a7cd9
creinis <115181527+creinis@users.noreply.github.com>
2024-07-31 08:55:18 +0200

    feat: .join() string method to convert the list of characters into a string

--- lecture.py
+++ lecture.py
@@ -77,5 +77,17 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
         else:
             snake_cased_char_list.append(char)
 
+# Step 8
+
+# By the end of the loop, snake_cased_char_list should contain all the converted characters in correct order. 
+# Use the .join() string method to convert the list of characters into a string.
+
+    # ''.join(snake_cased_char_list)
+
+# This joins the characters from the list to the empty string on which you called the .join() method. 
+# Save the result in a variable named snake_cased_string on the same level as the snake_cased_char_list variable.
+
+    snake_cased_string = ''.join(snake_cased_char_list)
+
 
 
