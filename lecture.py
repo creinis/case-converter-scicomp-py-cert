57fadb40fa61a1bb5dad54a0a27f48a2c929d99b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-31 08:45:12 +0200

    feat: defining function convert_to_snake_case() that accepts a string named pascal_or_camel_cased_string as input

---
+++ lecture.py
@@ -0,0 +1,16 @@
+# Case Converter Program
+
+# Step 1
+
+# In this project, you are going to learn about list comprehensions in Python by building a program that 
+# can take a camelCase or PascalCase formatted string and convert that to a snake_case formatted string.
+
+# List comprehensions in Python are a concise way to construct a list without using loops or the 
+# .append() method. Apart from being briefer, list comprehensions often run faster.
+
+# Start defining a new function named convert_to_snake_case() that accepts a string named 
+# pascal_or_camel_cased_string as input.
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    pass
+
