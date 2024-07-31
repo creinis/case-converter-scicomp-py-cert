396d53ebf729b4cb87ca9247fa07b064bfdd6022
creinis <115181527+creinis@users.noreply.github.com>
2024-07-31 08:55:57 +0200

    feat: .strip() string method and passing an underscore to the method as argument

--- lecture.py
+++ lecture.py
@@ -89,5 +89,20 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
 
     snake_cased_string = ''.join(snake_cased_char_list)
 
+# Step 9
+
+# Strings in camel case start with a capital character. 
+# Since you've converted all such characters to lowercase and prepended an underscore to them, 
+# chances are, the converted snake case string has a dangling underscode at the start.
+
+# The easiest way to strip such unwanted character is by using the .strip() string method and passing 
+# an underscore to the method as argument.
+
+#   snake_cased_string.strip('_')
+
+# Make sure to save the resulting string in a variable named clean_snake_cased_string on the same level 
+# as the snake_cased_string variable.
+
+    clean_snake_cased_string = snake_cased_string.strip('_')
 
 
