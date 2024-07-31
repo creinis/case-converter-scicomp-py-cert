02f2a5dbd5c33ec52a8a0828d60215422c285971
creinis <115181527+creinis@users.noreply.github.com>
2024-07-31 09:04:29 +0200

    feat: iterate upon all the characters of the string

--- lecture.py
+++ lecture.py
@@ -169,5 +169,24 @@ snake_cased_char_list = [
 
 # Add an else clause inside the pair of square braces.
 
+# Step 19
+
+# The final piece of the puzzle is the input string itself. 
+# The list comprehension needs to know about the object it'll iterate upon.
+
+# In this case, you need to iterate upon all the characters of the string.
+
+snake_cased_char_list = [
+    '_' + char.lower() if char.isupper()
+    else char
+    for char in pascal_or_camel_cased_string
+]
+
+# And there you have it. These three lines of code do the same task as the for loop you worked on previously 
+# while being cleaner and somewhat faster.
+
+# Add this last line of code to iterate over the characters of the string in your list comprehension 
+# and make sure that you're writing it within the pair of square braces.
+
 
 
