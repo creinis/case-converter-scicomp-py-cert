9f7ca222692097a687042c5ed895089b70938d70
creinis <115181527+creinis@users.noreply.github.com>
2024-07-31 08:45:57 +0200

    feat: create a new list snake_cased_char_list inside the function

--- lecture.py
+++ lecture.py
@@ -14,3 +14,14 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
     pass
 
+# Step 2
+
+# Now create a new list named snake_cased_char_list inside the function. 
+# You can use a set of empty square braces to create the new list.
+
+# This list will hold the characters of the string after you have converted them to snake case.
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = [] 
+
+
