0ddf17e794ee20ccfb2d1ab6e56b0c892dbab362
creinis <115181527+creinis@users.noreply.github.com>
2024-07-31 08:50:59 +0200

    feat: Inside the loop body an if statement in conjunction with the .isupper() string method to check for uppercase characters and move pass inside the new if statement

--- lecture.py
+++ lecture.py
@@ -37,6 +37,19 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     for char in pascal_or_camel_cased_string:
         pass
 
+# Step 4
+
+# Uppercase characters in camel case or pascal case indicate the start of new words.
+
+# Inside the loop body, use an if statement in conjunction with the .isupper() 
+# string method to check for uppercase characters and move pass inside the new if statement.
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+        if char.isupper():
+            pass
+
 
 
 
