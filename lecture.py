9868c42d20b29f67481116a34ff3088e1f80836c
creinis <115181527+creinis@users.noreply.github.com>
2024-07-31 08:52:13 +0200

    feat: convert any uppercase character to lowercase and prepend  an underscore to this lowercase character. The .lower() string method to convert uppercase characters to lowercase characters

--- lecture.py
+++ lecture.py
@@ -50,6 +50,21 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
         if char.isupper():
             pass
 
+# Step 5
+
+# Inside the if statement body, you need to convert any uppercase character to lowercase and prepend 
+# an underscore to this lowercase character.
+
+# Use the .lower() string method to convert uppercase characters to lowercase characters. 
+# You can then concatenate an underscore to the character using the plus sign.
+
+    #  '_' + char.lower()
+
+#Assign the modified character to a variable named converted_character inside the if statement body.
+
+        if char.isupper():
+            converted_character = '_' + char.lower()
+
 
 
 
